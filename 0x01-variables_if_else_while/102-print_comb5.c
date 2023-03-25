94dee6d0b58f18ee1c4d9a60168a5fcb1ff0568e
Paul Gomis <gomispapaul6@gmail.com>
2023-03-25 06:22:00 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,21 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i; j <= 99; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		putchar(',');
-		putchar(' ');
 	}
-	putchar('\n');
 	return (0);
-
 }
