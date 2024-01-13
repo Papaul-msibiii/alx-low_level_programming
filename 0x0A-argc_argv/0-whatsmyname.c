d15741a75046951e7e79e4c66c9b85159d03c1e2
Papaul-msibiii <gomispapaul6@gmail.com>
2024-01-13 07:18:36 -0800

    fix error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main (int argc, char *argv[])
+int main (int __attribute__((unused)) argc, char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
 	return 0;
 }
