64c8e089f8fa95d3d519bdbb617cd735f451a57c
Papaul-msibiii <gomispapaul6@gmail.com>
2024-02-16 09:28:24 -0800

    0. Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+void positive_or_negative(int i)
+{
+       if (i > 0)
+        {
+                printf("%d is positive", i);
+        } else if (i == 0)
+        {
+                printf("%d is zero", i);
+        } else
+        {
+                printf("%d is negative", i);
+        }
+        printf("\n");
+}
+
